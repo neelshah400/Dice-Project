1afc409d4460fd6f88faf780bd29407df36f9d0e
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2018-12-05 09:05:48 -0500

    Finish Die.java
    Class represents a single die

--- Die.java
+++ Die.java
@@ -1,5 +1,44 @@
 public class Die{
 
+	private int sides;
+	private int value;
 
+	public Die(){
+
+		sides = 6;
+		roll();
+
+	}
+
+	public Die(int sides){
+
+		this.sides = sides;
+		roll();
+
+	}
+
+	public int getSides(){
+
+		return sides;
+
+	}
+
+	public int getValue(){
+
+		return value;
+
+	}
+
+	public void roll(){
+
+		value = (int)(Math.random() * sides) + 1;
+
+	}
+
+	public String toString(){
+
+		return "Number of Sides: " + sides + ", Value: " + value;
+
+	}
 
 }
