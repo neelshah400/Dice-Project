e4083c6a8a914bbfd979b17dbabc01df14555a4e
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2018-12-05 09:24:29 -0500

    Add code to DiceRunner.java
    Start test 1

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,5 +1,29 @@
 public class DiceRunner{
 
+	public static void main(String[]args){
 
+		// Test 1
+
+		Die dice1 = new Die();
+		Die dice2 = new Die();
+
+		int rolls = 0;
+
+		System.out.println("Roll " + rolls + ":\n");
+		System.out.println("\t" + dice1);
+		System.out.println("\t" + dice2);
+
+		while(dice1.getValue != 1 && dice2.getValue != 1){
+
+			dice1.roll();
+			dice2.roll();
+
+			System.out.println("Roll " + rolls + ":\n");
+			System.out.println("\t" + dice1);
+			System.out.println("\t" + dice2);
+
+		}
+
+	}
 
 }
